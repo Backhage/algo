9e033c05207d1f2b5bda2beceed1465429c345ba
Eric Backhage <eric.backhage@gmail.com>
2013-10-19 22:10:50 +0200

    Added Quicksort as separate class.

---
+++ src/se/backhage/algo/QuickSort.java
@@ -0,0 +1,34 @@
+package se.backhage.algo;
+
+public class QuickSort {
+  public static void sort(int[] toSort, int minIndex, int maxIndex) {
+    int incIndex = minIndex;
+    int decIndex = maxIndex;
+    int pivotValue = toSort[(minIndex + maxIndex) / 2];
+    while (incIndex < decIndex) {
+      while (toSort[incIndex] < pivotValue) {
+        incIndex++;
+      }
+      while (toSort[decIndex] > pivotValue) {
+        decIndex--;
+      }
+      if (incIndex <= decIndex) {
+        swap(toSort, incIndex, decIndex);
+        incIndex++;
+        decIndex--;
+      }
+    }
+    if (incIndex < maxIndex) {
+      sort(toSort, incIndex, maxIndex);
+    }
+    if (decIndex > minIndex) {
+      sort(toSort, minIndex, decIndex);
+    }
+  }
+
+  private static void swap(int[] array, int firstIndex, int secondIndex) {
+    int tmp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = tmp;
+  }
+}
